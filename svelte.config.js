589e64c8ef0e35b9e6a76ddbbf1287413e4fc09a
Jacob Bashista <tgb.bashista@gmail.com>
2022-07-08 17:53:35 -0400

    Clean svelte project

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
+	}
+};
+
+export default config;
